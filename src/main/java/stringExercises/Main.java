dc264d89b5434e37549b7ad5ad5ac18149562f65
bianca-chira <bichira@deloittece.com>
2023-07-24 17:21:57 +0300

    String exercises 1,2,3

--- src/main/java/stringExercises/Main.java
+++ src/main/java/stringExercises/Main.java
@@ -15,8 +15,40 @@ public class Main {
         //Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
         System.out.println(StringExercises.firstHalf("WooHoo"));
 
+        //Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+        //nonStart("Hello", "There") → "ellohere"
+        System.out.println(StringExercises.nonStart("Hello", "There"));
+
+        //Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+        //theEnd("Hello", true) → "H"
+        System.out.println(StringExercises.theEnd("Hello",true));
+        System.out.println(StringExercises.theEnd("Hello", false));
+
+        //Given a string, return true if it ends in "ly".
+        System.out.println(StringExercises.endsLy("oddly"));
+        System.out.println(StringExercises.endsLy("Hello"));
+
+        //Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+        System.out.println(StringExercises.middleThree("Candy"));
+
+        //Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+        System.out.println(StringExercises.lastChars("last", "chars"));
+        System.out.println(StringExercises.lastChars("last", ""));
+        System.out.println(StringExercises.lastChars("", "chars"));
+        System.out.println(StringExercises.lastChars("", ""));
 
 
+        //Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+        System.out.println(StringExercises.seeColor("redxxsj"));
+        System.out.println(StringExercises.seeColor("blueiosa"));
+        System.out.println(StringExercises.seeColor("asredxx"));
+        System.out.println(StringExercises.seeColor("asblue"));
+
+
+        //Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+        System.out.println(StringExercises.extraFront("Hello"));
+        System.out.println(StringExercises.extraFront("H"));
+        System.out.println(StringExercises.extraFront("ab"));
 
     }
 }
--- src/main/java/stringExercises/StringExercises.java
+++ src/main/java/stringExercises/StringExercises.java
@@ -2,23 +2,87 @@ package stringExercises;
 
 public class StringExercises {
 
-    //helloName
     public static String helloName(String name) {
         return "Hello " + name + "!";
     }
 
-    //makeOutWord
     public static String makeOutWord(String out, String word) {
-        String firstWord = out.substring(0,2);
+        String firstWord = out.substring(0, 2);
         String endWord = out.substring(2);
         return firstWord + word + endWord;
     }
 
     public static String firstHalf(String str) {
-        int word = str.length() / 2;
-        return str.substring(0, word);
+        int halfWord = str.length() / 2;
+        return str.substring(0, halfWord);
 
+    }
 
+    public static String nonStart(String a, String b) {
+        return a.substring(1).concat(b.substring(1));
+    }
+
+    public static String theEnd(String str, boolean front) {
+        if (front) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
+
+    public static boolean endsLy(String str) {
+        return str.endsWith("ly");
+
+    }
+
+    public static String middleThree(String str) {
+        int numberChar = str.length();
+        int middle = numberChar / 2;
+        return str.substring(middle - 1, middle +2);
+
+    }
+
+    public static String lastChars(String a, String b) {
+        String first;
+        String last;
+        if (a.length() > 0){
+            first = a.substring(0,1);
+        }
+        else{
+            first = "@";
+        }
+        if (b.length() > 0){
+            last = b.substring(b.length()-1);
+        }
+        else{
+            last = "@";
+        }
+        return first + last;
+
+    }
+
+    public static String seeColor(String str) {
+        if(str.startsWith("red")){
+            return "red";
+        }
+        else if(str.startsWith("blue")){
+            return "blue";
+        }
+        else{
+            return "";
+        }
+
+    }
+
+    public static String extraFront(String str) {
+
+        if(str.length() >= 2){
+            String twoCh = str.substring(0,2);
+            return twoCh + twoCh +twoCh;
+        }
+        else{
+            return str + str +str;
+        }
     }
 
 }
